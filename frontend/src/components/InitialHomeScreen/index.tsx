8c2303969aa7b2adf42e767d2add4b857cd078cb
Muh-Hasan <muh.hasan.git@gmail.com>
2022-11-01 22:53:35 +0500

    update

--- frontend/src/components/InitialHomeScreen/index.tsx
+++ frontend/src/components/InitialHomeScreen/index.tsx
@@ -4,31 +4,53 @@ import "react-circular-progressbar/dist/styles.css";
 import CircularProgressBarWithLogo from "./CircularProgressBarWithLogo";
 import Button from "../common/Button";
 import Footer from "../Layout/Footer";
-import { useAccount, useConnectModal, useContract, useProvider, useSigner } from "@web3modal/react";
-import {Pledge, Details, contributedAmnt} from "../../web3functions/functions";
-const project = require("../../../../backend/deployments/polygon/Project.json")
-
+import {
+  useAccount,
+  useConnectModal,
+  useContract,
+  useProvider,
+  useSigner,
+} from "@web3modal/react";
+import {
+  Pledge,
+  Details,
+  contributedAmnt,
+} from "../../web3functions/functions";
+import { ethers } from "ethers";
+import moment from "moment";
+const project = require("../../../../backend/deployments/polygon/Project.json");
 
 const InitialHomeScreen = () => {
+  const [details, setDetails] = useState<any>();
+
+  const [date, setDate] = useState<any>(0);
+
   const { isOpen, open } = useConnectModal();
-  const {account, isReady} = useAccount()
-  const {provider } = useProvider()
-  const {data} = useSigner()
+  const { account, isReady } = useAccount();
+  const { provider } = useProvider();
+  const { data } = useSigner();
 
   const [stateChange, setStateChange] = useState(false);
   const [matic, setMatic] = useState("");
-  const [contributed, setContributed] = useState("0")
+  const [contributed, setContributed] = useState("0");
   const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
-  const NOW_IN_MS = new Date().getTime();
 
-  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
-  useEffect(()=>{
-    Details();
-  })
+  useEffect(() => {
+    (async () => {
+      setDetails(await Details());
+      if (details) {
+        setDate(
+          moment.unix(ethers.BigNumber.from(details.deadline._hex).toNumber())
+        );
+      }
+    })();
+  }, [details]);
 
   const handleStateChange = () => {
     if (account.isConnected) {
-      {console.log("Signer: ", data)}
+      {
+        console.log("Signer: ", data);
+      }
       setStateChange(true);
       //contributedAmnt(account.address, setContributed)
     } else {
@@ -38,21 +60,23 @@ const InitialHomeScreen = () => {
       }
     }
   };
+
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
       <CircularProgressBarWithLogo percentage={80} />
-      
+
       <div className="flex items-center gap-2 mt-4">
         <div className="w-6 h-6">
-          <img src="/assets/images/eth.svg" className="w-full h-full" />
+          <img src="/assets/images/matic.png" className="w-full h-full" />
         </div>
         <div className="uppercase text-lg font-medium text-gray-500">
-          100 eth
+          {details && Number(details?.amountToRaise._hex)}{" "}
+          Matic
         </div>
       </div>
 
       <div>
-        <Countdown targetDate={dateTimeAfterThreeDays} />
+        <Countdown targetDate={date} />
       </div>
 
       <div>
@@ -67,7 +91,12 @@ const InitialHomeScreen = () => {
               />
             </div>
             <div className="space-x-3">
-              <Button size="md" text="Pledge" withBg onClick={() =>Pledge(matic,account, data, isReady)}/>
+              <Button
+                size="md"
+                text="Pledge"
+                withBg
+                onClick={() => Pledge(matic, account, data, isReady)}
+              />
               <Button size="md" text="Unpledge" withBg disable={matic !== ""} />
             </div>
           </div>
